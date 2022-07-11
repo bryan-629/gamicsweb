da1cda77fa182ba685bbf157ce2b490266df9262
Bryan <Cruzbryan629@gmail.com>
2022-07-11 20:20:52 +0200

    iconos de ligas

--- components/DataLeagueCard.js
+++ components/DataLeagueCard.js
@@ -1,18 +1,101 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
 import useUtils from "../services/useUtils";
 import styles from "../styles/DataLeagueCard.module.css";
+import Image from "next/image";
 function DataLeagueCard({ userData, children }) {
   //userData.data.userDataBrCal.kd
   const [parseUser,getWeekTime,formatNumber] = useUtils();
+  const [leagueImg, setLeagueImg] = useState("/img/leagues/bronze1.png");
+
+  const getLeagueImg = (league) =>{
+    switch (league) {
+      case "BRONZE 4":
+        setLeagueImg("/img/leagues/bronze4.png")
+      break;
+      case "BRONZE 3":
+        setLeagueImg("/img/leagues/bronze3.png")
+      break;
+      case "BRONZE 2":
+        setLeagueImg("/img/leagues/bronze2.png")
+      break;
+      case "BRONZE 1":
+        setLeagueImg("/img/leagues/bronze1.png")
+      break;
+      case "SILVER 4":
+        setLeagueImg("/img/leagues/silver4.png")
+      break;
+      case "SILVER 3":
+        setLeagueImg("/img/leagues/silver3.png")
+      break;
+      case "SILVER 2":
+        setLeagueImg("/img/leagues/silver2.png")
+      break;
+      case "SILVER 1":
+        setLeagueImg("/img/leagues/silver1.png")
+      break;
+      case "GOLD 4":
+        setLeagueImg("/img/leagues/gold4.png")
+      break;
+      case "GOLD 3":
+        setLeagueImg("/img/leagues/gold3.png")
+      break;
+      case "GOLD 2":
+       setLeagueImg("/img/leagues/gold2.png")
+      break;
+      case "GOLD 1":
+        setLeagueImg("/img/leagues/gold1.png")
+      break;
+      case "PLATINUM 4":
+        setLeagueImg("/img/leagues/platinum4.png")
+      break;
+      case "PLATINUM 3":
+        setLeagueImg("/img/leagues/platinum3.png")
+      break;
+      case "PLATINUM 2":
+        setLeagueImg("/img/leagues/platinum2.png")
+      break;
+      case "PLATINUM 1":
+        setLeagueImg("/img/leagues/platinum1.png")
+      break;
+      case "DIAMOND 4":
+        setLeagueImg("/img/leagues/diamond4.png")
+      break;
+      case "DIAMOND 3":
+        setLeagueImg("/img/leagues/diamond3.png")
+      break;
+      case "DIAMOND 2":
+        setLeagueImg("/img/leagues/diamond2.png")
+      break;
+      case "DIAMOND 1":
+        setLeagueImg("/img/leagues/diamond1.png")
+      break;
+      case "MASTER":
+        setLeagueImg("/img/leagues/master.png")
+      break;
+      case "LEGEND":
+        setLeagueImg("/img/leagues/Leguend.png")
+      break;
     
+      default:
+        setLeagueImg("/img/leagues/bronze1.png")
+        break;
+    }
+  }
+  useEffect(()=>{
+    getLeagueImg(userData.data.userDataBrCal.userLeague)
+  },[])
   return (
     <div className={` p-16 mb-8 radius cardBackground`}>
       <div className="mb-8">
         <p className="overline">{children}</p>
       </div>
       <div className="d-flex al-center jus-center mb-16">
-        <div className="p-8">
-            <div className={`${styles.league}`}></div>
+        <div className="p-16">
+        <Image 
+            width={100}
+            height={42}
+            src={leagueImg}>
+            </Image>
         </div>
         <div className="jus-center ">
           <h5>{userData.data.userDataBrCal.userLeague}</h5>
--- components/DataLeagueCardWeek.js
+++ components/DataLeagueCardWeek.js
@@ -1,11 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useUtils from "../services/useUtils";
-
+import Image from "next/image";
 import styles from "../styles/DataLeagueCard.module.css";
 function DataLeagueCardWeek({userData,children}) {
 
   const [parseUser,getWeekTime,formatNumber] = useUtils();
+  const [leagueImg, setLeagueImg] = useState("/img/leagues/bronze1.png");
 
+  const getLeagueImg = (league) =>{
+    switch (league) {
+      case "BRONZE 4":
+        setLeagueImg("/img/leagues/bronze4.png")
+      break;
+      case "BRONZE 3":
+        setLeagueImg("/img/leagues/bronze3.png")
+      break;
+      case "BRONZE 2":
+        setLeagueImg("/img/leagues/bronze2.png")
+      break;
+      case "BRONZE 1":
+        setLeagueImg("/img/leagues/bronze1.png")
+      break;
+      case "SILVER 4":
+        setLeagueImg("/img/leagues/silver4.png")
+      break;
+      case "SILVER 3":
+        setLeagueImg("/img/leagues/silver3.png")
+      break;
+      case "SILVER 2":
+        setLeagueImg("/img/leagues/silver2.png")
+      break;
+      case "SILVER 1":
+        setLeagueImg("/img/leagues/silver1.png")
+      break;
+      case "GOLD 4":
+        setLeagueImg("/img/leagues/gold4.png")
+      break;
+      case "GOLD 3":
+        setLeagueImg("/img/leagues/gold3.png")
+      break;
+      case "GOLD 2":
+       setLeagueImg("/img/leagues/gold2.png")
+      break;
+      case "GOLD 1":
+        setLeagueImg("/img/leagues/gold1.png")
+      break;
+      case "PLATINUM 4":
+        setLeagueImg("/img/leagues/platinum4.png")
+      break;
+      case "PLATINUM 3":
+        setLeagueImg("/img/leagues/platinum3.png")
+      break;
+      case "PLATINUM 2":
+        setLeagueImg("/img/leagues/platinum2.png")
+      break;
+      case "PLATINUM 1":
+        setLeagueImg("/img/leagues/platinum1.png")
+      break;
+      case "DIAMOND 4":
+        setLeagueImg("/img/leagues/diamond4.png")
+      break;
+      case "DIAMOND 3":
+        setLeagueImg("/img/leagues/diamond3.png")
+      break;
+      case "DIAMOND 2":
+        setLeagueImg("/img/leagues/diamond2.png")
+      break;
+      case "DIAMOND 1":
+        setLeagueImg("/img/leagues/diamond1.png")
+      break;
+      case "MASTER":
+        setLeagueImg("/img/leagues/master.png")
+      break;
+      case "LEGEND":
+        setLeagueImg("/img/leagues/Leguend.png")
+      break;
+    
+      default:
+        setLeagueImg("/img/leagues/bronze1.png")
+        break;
+    }
+  }
+  useEffect(()=>{
+    getLeagueImg(userData.data.userDataBrWeekCal)
+  },[])
   return (
     <div className={` p-16 mb-8 radius cardBackground`}>
       <div className="mb-8">
@@ -13,8 +91,13 @@ function DataLeagueCardWeek({userData,children}) {
       </div>
       
       <div className="row al-center jus-center mb-16">
-        <div className="p-8">
-          <div className={` al-center ${styles.league}`}></div>
+        <div className="p-16">
+            <Image 
+
+            width={100}
+            height={42}
+            src={leagueImg}>
+            </Image>
         </div>
         <div className="al-center">
           <h5>{userData.data.userDataBrWeekCal}</h5>
